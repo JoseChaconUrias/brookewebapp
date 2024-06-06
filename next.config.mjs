c3cab4e7011d25ef198cde53489345bf1c7c7003
Jose Chacon <jcurias123@gmail.com>
2024-06-06 16:49:47 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV = 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 const nextConfig = {
     basePath: isProduction ? '/brookewebapp' : '',
     output: 'export',
