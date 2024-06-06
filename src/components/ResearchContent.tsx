cf1996ba1b8a83434f519e3316a90291b76246e8
Jose Chacon <jcurias123@gmail.com>
2024-06-06 17:30:59 -0500

    images for github fix

--- src/components/ResearchContent.tsx
+++ src/components/ResearchContent.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { ResearchCards } from './ui/ResearchCards'
-import Image from 'next/image'
+import React from "react";
+import { ResearchCards } from "./ui/ResearchCards";
+import Image from "next/image";
 
 const ResearchContent = () => {
   return (
     <div className="flex items-center justify-center h-screen p-8 lg:p-20">
       <div className="flex flex-col gap-4 lg:gap-20 w-full items-center">
-        <h2 className="text-2xl font-extrabold text-center lg:text-5xl">My Research Experience 🔬</h2>
+        <h2 className="text-2xl font-extrabold text-center lg:text-5xl">
+          My Research Experience 🔬
+        </h2>
         <div>
           <ResearchCards />
         </div>
-        <Image src="/kirby.webp" alt="kirby" width={300} height={300} />
-
+        <Image
+          src={`${
+            process.env.NODE_ENV === "production" ? "/brookewebapp" : ""
+          }/kirby.webp`}
+          alt="kirby"
+          width={300}
+          height={300}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResearchContent
+export default ResearchContent;
